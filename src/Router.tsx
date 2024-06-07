b8371df18c725e39f72220904a1fd344e3dc6e83
Sharhan123 <sharhanmohammed03@gmail.com>
2024-06-07 15:02:57 +0530

    trying deployment

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
 import UserRoutes from "./routers/userRouter";
 import AdminRoutes from "./routers/adminRouter";
 import DriverRoutes from "./routers/driverRouter";
