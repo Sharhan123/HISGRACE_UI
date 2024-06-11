3e5610fab8d7ee57b59c086b7a7eac44653f9a13
Sharhan123 <sharhanmohammed03@gmail.com>
2024-06-11 17:33:16 +0530

    vehicle card

--- src/components/customUI/customsButtons.tsx
+++ src/components/customUI/customsButtons.tsx
@@ -21,10 +21,10 @@ const CustomsButtons:React.FC<props> = ({first,second,secondFunction,image})=>{
     return (
     <div className=" flex justify-start items-start">
         <ModalImage closeModal={closeModal} open={modalOpen} image={image} />
-                                <button onClick={openModal} className=" bg-white kanit-regular  opacity-100 hover:opacity-100 text-black hover:text-gray-900 rounded-md px-5 py-2 font-semibold">
+                                <button onClick={openModal} className=" bg-white kanit-regular lg:text-md md:text-md text-xs opacity-100 hover:opacity-100 text-black hover:text-gray-900 rounded-md px-5 lg:py-2 md:py-2 py-1 ">
                                    {first} 
                                 </button>
-                                <button onClick={secondFunction} className="bg-red-600 ml-5 kanit-regular opacity-100 hover:opacity-100 text-white hover:text-gray-900 rounded-md px-5 py-2 font-semibold">
+                                <button onClick={secondFunction} className="bg-red-600 ml-5 kanit-regular lg:text-md md:text-md text-xs opacity-100 hover:opacity-100 text-white hover:text-gray-900 rounded-md px-5 lg:py-2 md:py-2 py-1">
                                     <i className="mdi mdi-cart -ml-2 mr-2"></i> {second}
                                 </button>
                             </div>
--- src/components/homePage/vehicleCard.tsx
+++ src/components/homePage/vehicleCard.tsx
@@ -11,14 +11,14 @@ const VehicleCard:React.FC<props> = ({vehicle,handleClick})=> {
     <div  className="relative overflow-hidden shadow-lg bg-white rounded-lg">
         <img onClick={() => handleClick(vehicle.id)} src={vehicle.images} alt={'hello'} className="object-contain" />
         <div className="absolute inset-0 flex flex-col justify-end p-4 bg-black bg-opacity-50">
-          <p className={`lg:h-5 md:h-3 w-fit px-2  rounded-lg text-xs kanit-regular flex items-center justify-center text-white ${vehicle.isBlocked ? 'bg-red-600' : 'bg-green bg-opacity-90'} text-center`}>
-            {vehicle.isBlocked ? 'Booked' : 'Available'}
-          </p>
-          <h1 onClick={() => handleClick(vehicle.id)} className=" lg:text-xl md:text-xl font-semibold text-white mb-1 kanit-bold">
+            {/* <p className={`lg:h-5 md:h-3 w-fit px-2  rounded-lg text-xs kanit-regular flex items-center justify-center text-white ${vehicle.isBlocked ? 'bg-red-600' : 'bg-green bg-opacity-90'} text-center`}>
+              {vehicle.isBlocked ? 'Booked' : 'Available'}
+            </p> */}
+          <h1 onClick={() => handleClick(vehicle.id)} className=" lg:text-xl md:text-xl text-sm font-semibold text-white mb-1 kanit-bold">
             {vehicle.vehicleName}
           </h1>
-          <p className="text-white mb-1 kanit-semibold">Premium {vehicle.seat} Seater {vehicle.type}</p>
-          <p className="text-white text-sm kanit-light">{vehicle.desc}</p>
+          <p className="text-white mb-1 lg:text-lg  md:text-lg text-xs kanit-semibold">Premium {vehicle.seat} Seater {vehicle.type}</p>
+          <p className="text-white lg:text-sm md:text-sm text-xs  kanit-light">{vehicle.desc}</p>
         <div className='mt-5'>
         <CustomsButtons image={vehicle.images}  secondFunction={()=>handleClick(vehicle.id)} first='View Image' second='View Details'/>
         </div>
